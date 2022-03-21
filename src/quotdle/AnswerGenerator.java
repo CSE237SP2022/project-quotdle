5c98d40bd117eeb1673f8d3f27faf19a4dcfd1a0
Akshat Sharma <akshatksharma@wustl.edu>
2022-03-21 15:40:27 -0500

    Initialized classes for game and wordle logic

---
+++ src/quotdle/AnswerGenerator.java
@@ -0,0 +1,10 @@
+package quotdle;
+
+public class AnswerGenerator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/quotdle/Wordle.java
@@ -0,0 +1,10 @@
+package quotdle;
+
+public class Wordle {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
