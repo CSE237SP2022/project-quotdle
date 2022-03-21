74b43d63897a737b1088f572e33a9b677d36944e
Daniel Goldberg <d.goldberg@wustl.edu>
2022-03-21 16:08:43 -0500

    Created LetterState and Wordle classes (at least bare bones outline of Wordle)

--- src/quotdle/LetterState.java
+++ src/quotdle/LetterState.java
@@ -1,10 +1,22 @@
 package quotdle;
 
+enum State {
+	correct, misplaced, wrong, blank
+}
+
 public class LetterState {
+	
+	char letter;
+	State state;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public LetterState() {
+		letter = 'a';
+		state = State.blank;
+	}
+	
+	public LetterState(char letter) {
+		letter = letter;
+		state = State.blank;
 	}
 
 }
--- src/quotdle/Wordle.java
+++ src/quotdle/Wordle.java
@@ -1,6 +1,29 @@
 package quotdle;
 
 public class Wordle {
+	
+	String answer;
+	int numberAllowedGuesses;
+	int currentGuess;
+	LetterState[][] currentGuesses;
+	LetterState[] keyboard;
+	
+	String getAnswer() {
+		return answer;
+	}
+	
+	LetterState[][] getGuesses() {
+		return currentGuesses;
+	}
+	
+	void submitGuess(LetterState[] guess) {
+		boolean didthiswork = processGuess(guess);
+	}
+	
+	boolean processGuess(LetterState[] guess) {
+		//Daniel: computation here
+		return false;
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
