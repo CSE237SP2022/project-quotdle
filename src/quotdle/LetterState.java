57336b3738ca8dee71c72468e89f2f7e71d01772
Daniel Goldberg <d.goldberg@wustl.edu>
2022-03-23 13:56:32 -0500

    on LetterState branch now. pushed LetterState class

--- src/quotdle/LetterState.java
+++ src/quotdle/LetterState.java
@@ -1,6 +1,27 @@
 package quotdle;
 
+enum States {
+	correct,
+	misplaced,
+	wrong,
+	blank
+}
+
 public class LetterState {
+	
+	char letter;
+	
+	States state;
+	
+	public LetterState() {
+		letter = 'a';
+		state = States.blank;
+	}
+	
+	public LetterState(char letter) {
+		letter = letter;
+		state = States.blank;
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
