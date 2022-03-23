5db88669a9889ceb8c05f6c19183bacd1841b860
Daniel Goldberg <d.goldberg@wustl.edu>
2022-03-23 14:12:42 -0500

    the bones of wordle class

--- src/quotdle/Wordle.java
+++ src/quotdle/Wordle.java
@@ -1,6 +1,31 @@
 package quotdle;
 
 public class Wordle {
+	
+	String answer;
+	
+	int numberAllowedGuesses;
+	int currentGuess;
+	
+	LetterState[][] currentGuesses;
+	LetterState[] keyboard;
+	
+	public String getAnswer() {
+		return answer;
+	}
+	
+	public LetterState[][] getGuesses() {
+		return currentGuesses;
+	}
+	
+	public void submitGuess(LetterState[] guess) {
+		processGuess(guess);
+	}
+	
+	private boolean processGuess(LetterState[] guess) {
+		
+		return false;
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
