414342b088a1993fadda00f962c724c820ab7c63
Akshat Sharma <akshatksharma@wustl.edu>
2022-03-26 18:01:51 -0500

    Created submitGuess function def

--- src/quotdle/Game.java
+++ src/quotdle/Game.java
@@ -1,4 +1,6 @@
 package quotdle;
+import javax.swing.text.html.FormSubmitEvent;
+
 import util.ArgsProcessor;
 
 public class Game {
@@ -10,8 +12,17 @@ public class Game {
 		currentWordleGame = new Wordle(AnswerGenerator.generateNewWordle(), 5);
 	}
 	
+	public boolean submitGuess(String guess) {   
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		ArgsProcessor ap = new ArgsProcessor(args);
+		String guess = ap.nextString("Provide a guess");
+		
+		Game currGame = new Game();
+		currGame.submitGuess(guess);
 
 	}
 
