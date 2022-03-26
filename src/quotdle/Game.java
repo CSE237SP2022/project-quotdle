455fcafa75c27a852b097fcab67ef8f5a1c75904
Akshat Sharma <akshatksharma@wustl.edu>
2022-03-25 22:46:36 -0500

    Setup constructor

--- src/quotdle/Game.java
+++ src/quotdle/Game.java
@@ -2,6 +2,13 @@ package quotdle;
 
 public class Game {
 
+	private Wordle currentWordleGame; 
+	
+	
+	public Game() {
+		currentWordleGame = new Wordle(AnswerGenerator.generateNewWordle(), 5);
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
